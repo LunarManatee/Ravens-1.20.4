9c6b20faeb4d937f0162d48018cac781cdc2a542
Mohamed Ahmed Wesam El-din <mw664746@gmail.com>
2024-04-26 16:51:56 +0300

    Semi-stable version of "Ravens-1.20.4"

--- src/main/java/com/lunar/tutorial/mixin/TitleScreenMixin.java
+++ src/main/java/com/lunar/tutorial/mixin/TitleScreenMixin.java
@@ -1,10 +1,8 @@
 package com.lunar.tutorial.mixin;
 
 import com.lunar.tutorial.custom.ModScreens.TutorialScreen;
-import net.minecraft.client.gui.widget.TextIconButtonWidget;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.gui.screen.world.SelectWorldScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
